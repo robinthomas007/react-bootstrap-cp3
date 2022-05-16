9565bdbbb31082638807b1e1eb12566a2c77dd96
Robin Thomas <Robin.Thomas@umusic.com>
2022-05-16 12:07:28 +0530

    policy page desing change

--- src/Components/Policy/index.js
+++ src/Components/Policy/index.js
@@ -80,33 +80,94 @@ export default function Policy() {
               </Col>
             </Row>
           </div>
-          <Row className="pt-20 policy-items">
-            <Col>
-              <strong>Policy:</strong> <span>Policy Name</span>
-            </Col>
-            <Col>
-              <strong>Platforms:</strong> <span>All</span>
-            </Col>
-            <Col>
-              <strong>Action:</strong> <span>Block</span>
-            </Col>
-            <Col>
-              <strong>Duration:</strong> <span>All</span>
-            </Col>
-            <Col>
-              <strong>Until:</strong> <span>Release Date (Default)</span>
-            </Col>
-          </Row>
+          <div className="create-policy-wrapper exceptions">
+            <Row>
+              <Col>
+                <div className="bg-secondary-light exception-field">Exception</div>
+              </Col>
+              <Col>
+                <Form.Group controlId="platform">
+                  <Form.Label>Platforms</Form.Label>
+                  <SelectField options={[]} isMulti={true} name="platform" handleChange={console.log()} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="action">
+                  <Form.Label>Action</Form.Label>
+                  <div className="d-flex">
+                    <Form.Check type='radio' label="Block" onChange={console.log()} />
+                    <Form.Check type='radio' label="Monetize" onChange={console.log()} />
+                  </div>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="duration">
+                  <Form.Label>Duration</Form.Label>
+                  <SelectField options={[]} isMulti={true} name="duration" handleChange={console.log()} />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="until">
+                  <Form.Label>Untill</Form.Label>
+                  <Form.Control type="date" name="untill" placeholder="Release Date" onChange={console.log()} />
+                </Form.Group>
+              </Col>
+            </Row>
+          </div>
           <hr />
-          <Row>
-            <Col md={10}>
-              <Button handleClick={() => { }} variant="secondary" startIcon={<AddCircleIcon />} label="Create Exception" className='text-white' />
-            </Col>
-            <Col md={2} className="d-flex justify-content-end">
-              <Button handleClick={() => { }} variant="secondary" label="Save" className='text-white mr-20' />
-              <Button handleClick={() => { }} variant="light" label="Cancel" className='' />
-            </Col>
-          </Row>
+        </Col>
+      </Row>
+      <Row className="pt-70">
+        <Col>
+          <h3 className="heading">Policy Summary</h3>
+          <hr />
+          <div className="">
+            <Row>
+              <Col>
+                <strong>Policy: </strong> <span>Policy Name</span>
+              </Col>
+              <Col>
+                <strong>Platforms: </strong> <span>All</span>
+              </Col>
+              <Col>
+                <strong>Action: </strong> <span>Block</span>
+              </Col>
+              <Col>
+                <strong>Duration: </strong> <span>All</span>
+              </Col>
+              <Col>
+                <strong>Until: </strong> <span>Release Date (Default)</span>
+              </Col>
+            </Row>
+          </div>
+          <div className="create-policy-wrapper exception-summary">
+            <Row className="align-items-center">
+              <Col>
+                <div className="bg-secondary-light exception-summary">Exception</div>
+              </Col>
+              <Col>
+                <strong>Platforms :</strong><span>YouTube</span>
+              </Col>
+              <Col>
+                <strong>Action :</strong><span>Monetize</span>
+              </Col>
+              <Col>
+                <strong>Duration :</strong><span> Less than 30 seconds</span>
+              </Col>
+              <Col>
+                <strong>Untill :</strong><span>Sep 6, 2022</span>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={10}>
+          <Button handleClick={() => { }} variant="secondary" startIcon={<AddCircleIcon />} label="Create Exception" className='text-white' />
+        </Col>
+        <Col md={2} className="d-flex justify-content-end">
+          <Button handleClick={() => { }} variant="secondary" label="Save" className='text-white mr-20' />
+          <Button handleClick={() => { }} variant="light" label="Cancel" className='' />
         </Col>
       </Row>
     </div>
