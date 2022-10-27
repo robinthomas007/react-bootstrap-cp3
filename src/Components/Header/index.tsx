574bfbcd6ff3df0670b2ae9abfd0d956ae39eaf2
Robin Thomas <Robin.Thomas@umusic.com>
2022-10-27 13:24:11 +0530

    notifications changes

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./../../Static/Images/logo.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,14 +9,41 @@ import LightModeIcon from "@mui/icons-material/LightMode";
 import { useColor } from "../../Context/ColorModeContext";
 import { useAuth } from "./../../Context/authContext";
 import Nav from "react-bootstrap/Nav";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
 import ApprovalIcon from "@mui/icons-material/Approval";
 import PolicyIcon from '@mui/icons-material/Policy';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Loader from "./../Common/loader";
+import axios from "axios";
+import { BASE_URL } from "./../../App";
+import { config } from "./../Common/Utils";
+import moment from 'moment';
+import './header.css'
+
+const hexArray = [
+  '#FF26A8',
+  '#26A4FF',
+  '#CE53FA',
+  '#609491',
+  '#26A4FF',
+  '#FF26A8',
+  '#26A4FF',
+  '#CE53FA',
+  '#d4953c',
+  '#609491',
+  '#26A4FF',
+  '#CE53FA',
+];
 
 export default function Header() {
   const colorModeContext = useColor();
   const auth = useAuth();
+  const navigate = useNavigate();
+  const [notifications, setNotifications] = useState([]);
+  const [showNoti, setShowNoti] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     if (colorModeContext.colorMode === "light") {
       document.body.classList.add("light-theme");
@@ -26,6 +53,86 @@ export default function Header() {
       document.body.classList.remove("light-theme");
     }
   }, [colorModeContext.colorMode]);
+
+  useEffect(() => {
+    getAllNotifications();
+    const interval = setInterval(() => {
+      getAllNotifications();
+    }, 50000);
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      clearInterval(interval)
+    }
+  }, [])
+
+  const handleClickOutside = (event: any) => {
+    const elem = document.querySelector('.notification-wrapper-div');
+    if (elem && !elem.contains(event.target) && (event.target.id !== 'notify-wrapper' && event.target.parentElement.id !== 'notify-wrapper')) {
+      if (document.querySelector('.notification-wrapper')) {
+        setShowNoti(false)
+        event.preventDefault();
+      }
+    }
+  }
+
+  const getAllNotifications = () => {
+    axios
+      .post(BASE_URL + "Notification/GetUnReadNotification", {}, config)
+      .then((response) => {
+        if (response.data && response.data.length > 0) {
+          setNotifications(response.data)
+        }
+      })
+      .catch((err) => {
+
+      })
+      .finally(() => { });
+  }
+
+  const openNotification = () => {
+    setShowNoti(!showNoti)
+  }
+
+  const getAlias = (name: any) => {
+    if (name) {
+      const nameArr = name.split(' ');
+      return nameArr[0].charAt().toUpperCase() + nameArr[0].charAt(1).toUpperCase();
+    }
+    return 'UK';
+  };
+
+  const markAsRead = (id: number) => {
+    setLoading(true)
+    axios
+      .post(BASE_URL + "Notification/ReadNotification", { notificationId: id }, config)
+      .then((response) => {
+        if (response.status === 200) {
+          setLoading(false)
+          navigate("/first_seen");
+        }
+      })
+      .catch((err) => {
+        setLoading(false)
+      })
+      .finally(() => {
+        setLoading(false)
+      });
+  }
+
+  const renderNotifications = () => {
+    return notifications.map((noti: any, i) => {
+      return (
+        <div key={i} className="noti-item">
+          <div className="alias"><span style={{ background: hexArray[i] }}> {getAlias(noti.userName)}</span></div>
+          <div className="noti-content" onClick={() => markAsRead(noti.notificationId)}>
+            <strong>{noti.userName}</strong> {noti.notificationType.toLowerCase()} the first scene record <strong>"{noti.trackName}"</strong>
+            <span> ({moment.utc(noti.createdDateTime).fromNow()})</span>
+          </div>
+        </div>
+      )
+    })
+  }
 
   const adminNavLinks = [
     {
@@ -37,6 +144,7 @@ export default function Header() {
 
   return (
     <Container fluid>
+      {loading && <Loader />}
       <Row className="bg-header-theme text-white cp3-header">
         <Col xl={3} xxl={5}>
           <a href="/">
@@ -66,6 +174,21 @@ export default function Header() {
                   </NavLink>
                 </Nav.Item>
               ))}
+            <Nav.Item>
+              <Nav.Link>
+                <div>
+                  <div className="notify-wrapper" id="notify-wrapper">
+                    <NotificationsIcon id="notify-wrapper" onClick={openNotification} className="noti-icon" />
+                    {notifications.length > 0 && <span className="noti-count">{notifications.length}</span>}
+                  </div>
+                  <div className="notification-wrapper-div">
+                    {showNoti && <div className="notification-wrapper arrow-top">
+                      {renderNotifications()}
+                    </div>}
+                  </div>
+                </div>
+              </Nav.Link>
+            </Nav.Item>
             <Nav.Item>
               <Nav.Link>
                 <span onClick={colorModeContext.toggleColorMode}>
